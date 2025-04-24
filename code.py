e332b81f9cad3be2583a570c6fbf954ddd95f549
SLM-LMS <selim.olmus@horoz.com.tr>
2025-04-24 14:11:31 +0300

    Update code.py

--- code.py
+++ code.py
@@ -5,7 +5,7 @@ import openpyxl
 from openpyxl import load_workbook
 import os
 
-excel_path = 'Data.xlsx'
+excel_path = r"C:\\Users\selim.olmus\\OneDrive - Horoz Lojistik\\_slms_\\Opet_Yakıt\\Data2.xlsx"
 
 # ---- BÖLÜM 1: VERİ ÇEKME VE GÜNCELLEME ----
 
